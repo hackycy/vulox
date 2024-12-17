6c01ec4cb83fd01b1d635af3e696a75a952a76c3
Yuan <qa894178522@qq.com>
2024-12-17 23:59:45 +0800

    wip

--- lib/module-loader.ts
+++ lib/module-loader.ts
@@ -1,9 +1,16 @@
 import type { Module, Plugin, Result, VOptions } from './types'
 
 export class ModuleLoader {
+  private modules: Map<string, Module> = new Map()
   private options: VOptions
 
   constructor(options: VOptions) {
     this.options = options
   }
+
+  public async load(_id: string = this.options.entry): Promise<void> {
+    await this.resolveModule(_id, true)
+  }
+
+  private async resolveModule(id: string, _isEntry = false, _importer?: string): Promise<void> {}
 }
